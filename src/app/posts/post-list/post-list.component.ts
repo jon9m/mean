ff81f5722076245b48dacab23e46845e239390bf
jon <manojam79@gmail.com>
2019-01-13 00:52:59 +1100

    Angular Front-end

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Post } from '../post.model';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+
+  @Input() posts: Post[] = [
+    // { title: 'First post', content: 'This is the first post' },
+    // { title: 'Second post', content: 'This is the second post' },
+    // { title: 'Third post', content: 'This is the third post' }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
